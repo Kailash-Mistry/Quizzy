a9e02778509ebee6fdaee38e9eeebba987ed43d4
Vinayak Gupta <vinayak20.in@gmail.com>
2024-02-03 16:02:44 +0530

    Completed the main page, added footer

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Main } from './MainSection'
 import { Main2 } from './MainSection2'
 import CarouselDemoAutoPlay from './carousel'
 import { Details } from './MainSection3'
+import { Foot } from './footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Main2/>
       {/* <CarouselDemoAutoPlay/> */}
       <Details/>
+      <Foot/>
     </>
   </React.StrictMode>,
 )
